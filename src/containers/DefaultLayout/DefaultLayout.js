541c82fb6028447517e4efd03f31f6f97cd13d24
lucaslpsilveira <lucas.lpsilveira@gmail.com>
2020-06-24 19:26:51 -0300

    api allow cors and others

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -29,7 +29,7 @@ class DefaultLayout extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
               <li className="nav-item active">
-                <a clclassNameass="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/">Link</a>
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+axios.defaults.headers['Content-Type'] = 'application/json';
+axios.defaults.headers['crossDomain'] = true;
+axios.defaults.headers['withCredentials'] = true;
+axios.defaults.headers['use-redirect'] = false;
 const api = axios.create({
     baseUrl: process.env.REACT_APP_API_URL
 });
