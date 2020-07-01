348d73a76629bf02398b3f39890a51b48fcc7cdc
lucaslpsilveira <lucas.lpsilveira@gmail.com>
2020-07-01 00:29:59 -0300

    range picker para visualizar vigencia dos contratos

--- src/views/DistInfo/DistInfo.js
+++ src/views/DistInfo/DistInfo.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import { Row, Col, Card, CardBody, CardHeader, Input } from 'reactstrap';
 import api from '../../services/api';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
 
-class Cidades extends Component {
+class DistInfo extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       formType: '',
       info: [],
+      toSearch: [],
+      start_date: null,
+      end_date: null,
       loading: true
     }
   }
 
-  async getResults(){         
+  async onChangeDate(name, date) {    
+    if (date[0] && date[1]) {            
+      let start_date = date[0]._d.toISOString().slice(0,10);
+      let end_date = date[1]._d.toISOString().slice(0,10);
+      await this.setState({ start_date });
+      await this.setState({ end_date });
+      this.getResults('&vigencia='+start_date+'/'+end_date);
+    } else {
+      await this.setState({ start_date: null });
+      await this.setState({ end_date: null });
+      this.getResults();
+    }
+  }
+
+  async getResults(search = ''){         
       let state = this.props.match.params.id;
-      api.get('/city/distribuidoras/?distribuidora='+state)
+      api.get('/city/distribuidoras/?distribuidora='+state+search)
       .then(async res => {
         console.log('processing',res);        
         let info = res.data;
@@ -38,29 +57,16 @@ class Cidades extends Component {
   </Row>;
 
   render() {    
+    const { RangePicker } = DatePicker;
+    const dateFormat = 'YYYY-MM-DD';
+
     return (
       <>
         { this.state.loading ? this.loading() :
-          <>
-             {/*<Row className='mt-2 mb-2'>
-              <Col md='4'>
-                <label>CNPJ:</label>
-                <Input/>
-              </Col>
-              <Col md='4'>
-                <label>Distribuidora:</label>
-                <Input/>
-              </Col>
-              <Col md='4'>
-                <label>Vigencia:</label>
-                <Input/>
-              </Col>
-        </Row>*/}
-            <hr/>
-            <Row>
+          <>                        
               {Array.isArray(this.state.info)
                 ? <>{this.state.info.map((line, i) => {
-                  return <><Col md='2' className='mt-2 mb-2' key={i}>
+                  return <Row key={i}><Col md='2' className='mt-2 mb-2'>
                     <Card>
                       <CardHeader><b>{line.distribuidora}</b></CardHeader>            
                       <CardBody>
@@ -71,11 +77,22 @@ class Cidades extends Component {
                   </Col>
                   <Col md='10' className='mt-2 mb-2' key={i}>
                     <Card>
-                      <CardHeader><b>Cidades Atendidas</b></CardHeader>            
+                      <CardHeader>
+                        <Row>
+                          <Col sm='12' md='8'><b>Cidades Atendidas</b></Col>                          
+                          <Col sm='12' md='4'>
+                            <RangePicker 
+                              format={dateFormat}
+                              defaultValue={[this.state.start_date, this.state.end_date]}
+                              placeholder={['Vigencia inicial', 'Vigencia final']}
+                              onChange={(e) => this.onChangeDate('date', e)} />
+                          </Col>
+                        </Row>
+                      </CardHeader>            
                       <CardBody>
                         <Row>
-                        {line.cityInfo.map(city =>{
-                          return <Col md='4' className='mb-2'>
+                        {line.cityInfo.length > 0 ? line.cityInfo.map((city,j) =>{
+                          return <Col key={j} md='4' className='mb-2'>
                             <Card>
                               <CardHeader>
                                 <h4>{city.nome}</h4>
@@ -87,14 +104,13 @@ class Cidades extends Component {
                               </CardBody>
                             </Card>                                                                                    
                           </Col>
-                        })}
+                        }): <h4>Nenhuma cidade com vigencia para o range selecionado!</h4>}
                         </Row>
                       </CardBody>
                     </Card>
-                  </Col> </> 
+                  </Col> </Row> 
                 })}</> 
-                : <Col className='text-center mt-4'><h4>Nothing to show!</h4></Col>}              
-            </Row>            
+                : <Row><Col className='text-center mt-4'><h4>Nothing to show!</h4></Col></Row>}                          
           </>
         }
       </>
@@ -102,4 +118,4 @@ class Cidades extends Component {
   }
 }
 
-export default Cidades;
+export default DistInfo;
